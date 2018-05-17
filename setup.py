472d3bd2ae5f88d55eb82a16556ec5e4758a9769
garrett <geickelb@github.com>
2018-05-17 17:58:41 -0500

    Create initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='my work with Dr. Yuan Luo focuses on modeling antibiotic time courses for patients admitted to the ICU based on a variety of clinical parameters using data from the MIMICiii ICU database',
+    author='Garrett Eickelberg',
+    license='MIT',
+)
